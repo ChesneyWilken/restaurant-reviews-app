1546f054e77795b554d7cc67d58733d9c7962992
Chesney <chezwilken@yahoo.co.uk>
2019-07-18 20:47:31 +0100

    Fix
    Restructured layout

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 // register service worker
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-  .then(function(reg) {
-    // registration worked
-    console.log('Registration succeeded.' + reg.scope);
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(reg) {
+        // registration worked
+        console.log('Registration succeeded.' + reg.scope);
+      }).catch(function(error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
+      });
+    }
+  )
 };
 
 
