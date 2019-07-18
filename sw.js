6088eb044e51097739ef321d9547d0cb93a099ce
Chesney <chezwilken@yahoo.co.uk>
2019-07-18 20:42:35 +0100

    Fix
    - Fixed error with cache not storing information
    - Added additional cache for new resources

--- sw.js
+++ sw.js
@@ -1,27 +1,41 @@
-let appCache;
 //install for the service worker
 self.addEventListener('install', function(event){
   event.waitUntil(
-    caches.open('appCache').then(function(cache){
+    caches.open('app-v1').then(function(cache){
       return cache.addAll([
         './',
-        './css/',
-        './data/',
-        './img/',
-        './js/',
+        './css/styles.css',
+        './data/restaurants.json',
+        './img/1.jpg',
+        './img/2.jpg',
+        './img/3.jpg',
+        './img/4.jpg',
+        './img/5.jpg',
+        './img/6.jpg',
+        './img/7.jpg',
+        './img/8.jpg',
+        './img/9.jpg',
+        './img/10.jpg',
+        './js/dbhelper.js',
+        './js/main.js',
+        './js/restaurant_info.js',
         './index.html',
-        './restaurant.html'
+        './restaurant.html',
       ]);
     })
   );
 });
 
-// look for a match in the cache, if no results found go to the network
-self.addEventListener('fetch', function(event){
+// get files from cache - look for a match in the cache, if no results found go to the network
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response){
-      if(response) return response;
-      return fetch(event.request);
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).then(function(response) {
+        return caches.open('app-v1').then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
     })
   );
 });
